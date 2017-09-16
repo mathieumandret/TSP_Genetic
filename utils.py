7bad7927ea437e76ec0a074e9b70f5732336224f
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-09-16 15:36:20 +0200

    fonction get_weight() fonctionnelle

--- utils.py
+++ utils.py
@@ -6,35 +6,43 @@ def detail_path(string_path):
     """
     glob_list = []
     i = 0
-    while(i < len(string_path)-1):
-        tmp_list = [int(string_path[i]), int(string_path[i+1])]
+    while (i < len(string_path) - 1):
+        tmp_list = [int(string_path[i]), int(string_path[i + 1])]
         glob_list.append(tmp_list)
         i += 1
     return glob_list
 
+
 def init_map():
     """
-    Cree une matrice d'incidence du graphe 
+    Cree une matrice d'incidence du graphe
     de la carte de villes
     """
-    return [[-1,1,2], [1,-1,4], [2,4,-1]]
+    return [[-1, 1, 2], [1, -1, 4], [2, 4, -1]]
+
 
 def get_weight(string_path):
     """
     Retourne le poids d'un chemin donne
     par un string de la forme '1234'
     """
-
-    #Pour paire de sommets, on doit ajouter a la somme 
+    if (len(string_path) > len(init_map()[0])):
+        raise ValueError('Le chemin demande comporte plus de ville que la carte')
+    somme = 0
+    #Pour paire de sommets, on doit ajouter a la somme
     #la valeur de matrix[sommet1-1][sommet2-1]
 
     matrix = init_map()
     weight = 0
     paths = (detail_path(string_path))
+    #Parcours des couples de sommets
     for path in paths:
-        print(path)
+        for i in range(len(path) - 1):
+            somme += matrix[path[i] - 1][path[i - 1] - 1]
+    return somme
+
 
 #Cette fonction devrai retourner
 # 1->2 2->3
 # 1 + 4 = 5
-get_weight('123')
+print(get_weight('123'))
