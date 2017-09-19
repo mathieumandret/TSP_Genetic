88adff98c572d923144ea7e1631817a883545347
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-09-19 17:18:00 +0200

    Crossover dict

--- crossover.py
+++ crossover.py
@@ -9,18 +9,41 @@ b = [4, 1, 2, 3]
 gauche = 1
 droite = 3
 
-print(a,b)
 #Swap
 a[gauche:droite], b[gauche:droite] = b[gauche:droite],a[gauche:droite]
 
-print(a,b)
-
 #Echange effectues
-echanges = {}
+echangesB = {}
+echangesA = {}
+
 for i in range(len(b[gauche:droite])):
-    echanges[a[gauche:droite][i]] = b[gauche:droite][i]
-    echanges[b[gauche:droite][i]] = a[gauche:droite][i]
+    echangesB[a[gauche:droite][i]] = b[gauche:droite][i]
 
-print(echanges)
+for i in range(len(a[gauche:droite])):
+    echangesA[b[gauche:droite][i]] = a[gauche:droite][i]
 
-#Verifier les elements dupliques et utiliser le dictionnaire echanges pour les corriger
+def estLegal(chemin):
+    """
+    Verifie si un chemin contient ou non 
+    des elements dupliques
+    """
+    return [el for el in chemin[gauche:droite] if el in (chemin[:gauche]+chemin[droite:])] == []
+
+#Partie exterieure de la decoupe
+partex = a[::(droite-gauche)+1] # = a[:gauche]+a[droite:]
+#Partie interieur de la decoupe
+partint = a[gauche:droite]
+
+print(partex, partint)
+
+dup = []
+
+#Detection de l'indice des elements duplique
+for i in range(len(partex)):
+    if partex[i] in partint:
+        dup.append(partex[i])
+
+print("Elements dupliques: ",dup)
+print("A remplacer par", echangesB[dup[0]])
+
+
