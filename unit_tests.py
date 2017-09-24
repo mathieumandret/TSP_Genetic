237ea6968e908cfb7fbd160c8cd6a4448a9b09a6
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-09-24 15:12:36 +0200

    Nettoyage

--- unit_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from creation_chemins import mutate
-
-class PathMethodsUnitTest(unittest.TestCase):
-    def test_mutate(self):
-        self.assertTrue(liste == copy)
-
-unittest.main()
--- utilClass.py
+++
@@ -1,4 +0,0 @@
-class Utilies:
-
-    def __init__(self):
-
