965374859bff2df09ae8f47c980744f4c95a38e9
Mathieu Mandret <mathieu.mandret119@gmail.com>
2017-11-10 17:07:52 +0100

    argparse -> optparse + nouvelles options

--- main.py
+++ main.py
@@ -3,43 +3,52 @@
 from Population import Population
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-import argparse
+import optparse
 
-parser = argparse.ArgumentParser(
-    description='gere le fichier carte passé en argument.')
-parser.add_argument('carte')
-args = parser.parse_args()
+# Gestionnaire d'option
+parser = optparse.OptionParser()
+parser.add_option('-c', '--carte', action='store',
+                  dest='carte', default='cercle.csv',
+                  help='carte des villes à relier')
+parser.add_option('-n', '--individus', action='store',
+                  dest='nb_inds', default=50,
+                  help='nombre d\'individus dans la population')
+parser.add_option('-m', '--mutation', action='store',
+                  dest='mut_rate', default=2,
+                  help='fréquence de mutation')
+parser.add_option('-g', '--generation', action='store',
+                  dest='nb_gens', default=100,
+                  help='nombre de générations')
+parser.add_option('-t', '--tournoi', action='storeTrue', dest='sel_tournoi',
+                  help='utiliser la méthode de selection par tournoi')
+parser.add_option('-r', '--roulette', action='storeTrue', dest='sel_roulette',
+                  help='utiliser la méthode de selection par roulette')
+parser.add_option('-e', '--elitisme', action='storeTrue', dest='elitisme',
+                  help='garder les meilleurs parents à chaque génération')
+
+options, args = parser.parse_args()
 
 
 def animer(i):
     """
     Met à jour le graphe du meilleur chemin de chaque population
     """
-    p1.evoluer(MUT_1)
+    p1.evoluer(options.mut_rate)
     nx1, ny1 = p1.meilleurChemin.to_plot()
     plt.title('Génération: ' + str(p1.generation))
     graph1.set_data(nx1, ny1)
 
 
-# Paramètres des populations
-NBINDIVIDUS = 50
-MUT_1 = 5
-NB_GENS = 1000
-# Création de 2 populations identiques de 100 individus
-p1 = Population(NBINDIVIDUS, 0, args.carte)
+p1 = Population(options.nb_inds, 0, options.carte)
 init_dist = p1.meilleurFitness
-# Meilleurs chemins des 2 population
 x1, y1 = p1.meilleurChemin.to_plot()
-# Préparation du graphe
 fi = plt.figure()
 plt.title('Génération: 1')
 plt.axes().set_aspect('equal', 'datalim')
-# Affichage des villes
 plt.scatter(x1, y1)
-# Affichage du meilleur element deux populations
 graph1, = plt.plot(x1, y1, 'r')
-# Animation
-ani = FuncAnimation(fi, animer, interval=10, repeat=False, frames=NB_GENS - 2)
+ani = FuncAnimation(fi, animer, interval=10, repeat=False,
+                    frames=options.nb_gens - 2)
 plt.show()
 plt.close()
 end_dist = p1.meilleurFitness
